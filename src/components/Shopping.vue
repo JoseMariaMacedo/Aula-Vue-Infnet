463e8625044acf529f5514670e898b1f2db2a245
José Maria Macedo <jose_macedo@live.com>
2022-02-13 17:55:24 -0300

    Create Shopping
    Criando o Shopping e toda estrutura do Template

---
+++ src/components/Shopping.vue
@@ -0,0 +1,73 @@
+<template>
+    <div>
+            <h1>{{ name_store || "My store" }}</h1>
+            <h2 v-html="subTittle"></h2>
+
+            <button
+                @click="insert = !insert">Inserir</button>
+            <div v-show="insert">
+                <input type="text"
+                @keyup.enter = "item.push({...newItem})"
+                v-model = "newItem.product">
+
+                <strong>Priority:</strong>
+                <label>
+                    <input type="radio" v-model="newItem.priority" value="high">High
+                </label>
+
+                <label>
+                    <input type="radio" v-model="newItem.priority" value="low">Low
+                </label>
+
+                <label>
+                    <strong>Color:</strong>
+                    <select v-model="newItem.color">
+                        <option value="black">Black</option>
+                        <option value="blue">Blue</option>
+                        <option value="Pink">Pink</option>
+                    </select>
+                </label>
+
+                <label>
+                    <strong>Gift</strong>
+                    <input type="checkbox" v-model="newItem.Gift">
+                    Is a Gift?
+                </label>
+
+                <label>
+                    <strong>Complements</strong>
+                    <input type="checkbox" v-model="newItem.complements" value="giftcard">
+                    Gift card?
+                    <input type="checkbox" v-model="newItem.complements" value="tracking">
+                    Tracking?
+                </label>
+
+                <br>
+                <button
+                    :disabled="newItem.product.length < 3 "
+                    @click="items.push({ ...newItem })">
+                    Save item
+                </button>
+
+            </div>
+            <h3>items
+                <span v-show="items.length > 0">
+                    {{ items.length }}
+                </span>    
+            </h3>
+
+            <p v-if="items.length === 0 ">No items</p>
+            <div v-else-if="items.length === 1 ">1 Item</div>
+            <span v-else>{{ items.length }} Items </span>
+
+            <ul>
+                <li
+                    v-for="item in items"
+                    :key="item.id">
+                    <img v-bind="item.img" width="56">
+                    {{ item.product }}
+                    {{ item.price }}
+                </li>
+            </ul>
+    </div>
+</template>
